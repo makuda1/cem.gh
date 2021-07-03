cb97f845f66834297b1b22610e6079d4114a4d4d
makuda1 <86896916+makuda1@users.noreply.github.com>
2021-07-03 23:54:35 +0000

    new files added

---
+++ cman/admin/footer.php
@@ -0,0 +1,16 @@
+<div class="setfooter">
+		<footer >
+           <p align="center"><h3 align="center"> &copy; Christ Exploit Ministry <?php
+ $date = new DateTime();
+ echo $date->format(' Y');
+ ?> </h3></p>
+		
+		  		  <h3 align="center"> Follow Us :</h3> <p align="center">
+<a href="https://web.facebook.com/bright.gyamfi" ><img src="images/facebook.png" id=social /></a>
+<a href="#"><img src="images/twitter.png" id=social /></a>
+<a href="#"><img src="images/linkedin.png"id=social  /></a>
+<a href="#"><img src="images/youtube.png" id=social /></a>
+<a href="#"><img src="images/gmail.png" id=social /></a></p>
+</div>
+        <footer>
+</div>
